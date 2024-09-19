b953bb30171e14462e47ee808dc285f913d68d3f
Daniel Major <daniel.major@fiserv.com>
2024-09-18 21:42:06 -0300

    Paginação

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,15 +1,14 @@
 import { Helmet } from "react-helmet-async";
-import { Input } from "../../../components/ui/input";
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "../../../components/ui/table";
-import { Button } from "../../../components/ui/button";
-import { ArrowRight, Search, X } from "lucide-react";
+import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filter";
+import { Pagination } from "../../../components/pagination";
 
 export function Orders() {
   return (
@@ -17,66 +16,29 @@ export function Orders() {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros:</span>
-          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-        </form>
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]"></TableHead>
-                <TableHead className="w-[140px]">Identificador</TableHead>
-                <TableHead className="w-[180px]">Realizando há</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Cliente</TableHead>
-                <TableHead className="w-[140px]">Total do pedido</TableHead>
-                <TableHead className="w-[164px]"></TableHead>
-                <TableHead className="w-[132px]"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Button className="outline" size="xs">
-                    <Search className="W-3 h-3" />
-                    <span className="sr-only">Detalhes do pedido</span>
-                  </Button>
-                </TableCell>
-                <TableCell className="font-mono text-xs font-medium">
-                  1
-                </TableCell>
-                <TableCell className="text-muted-foreground">
-                  há 15 minutos
-                </TableCell>
-                <TableCell>
-                  <div className="flex items-center gap-2">
-                    <span className="h-2 w-2 rounded-full bg-slate-400" />
-                    <span className="font-medium text-muted-foreground">
-                      Pendente
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell className="font-medium">Daniel Major</TableCell>
-                <TableCell className="font-medium">R$ 149,90</TableCell>
-                <TableCell>
-                  <Button variant="outline" size="xs">
-                    <ArrowRight className="mr-2 h-3 w-3" />
-                    Aprovar
-                  </Button>
-                </TableCell>
-                <TableCell>
-                  <Button variant="ghost" size="xs">
-                    <X className="mr-2 h-3 w-3" />
-                    Cancelar
-                  </Button>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizando há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <OrderTableRow />
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
