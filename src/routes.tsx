1a5eba6370ac978b95a46786c75ec078f3f8f4e2
Daniel Major <daniel.major@fiserv.com>
2024-10-03 11:12:38 -0300

    Dashboard

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import { SignIn } from "./pages/auth/sign-in";
-import { Dashboard } from "./pages/app/dashboard";
+import { Dashboard } from "./pages/app/dashboard/dashboard";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { AppLayout } from "./pages/_layouts/app";
 import { SignUp } from "./pages/auth/sign-up";
