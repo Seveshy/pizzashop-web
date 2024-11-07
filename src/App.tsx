4d6442fc9dfb6daac9bde846e47382e8a614193f
Daniel Major <daniel.major@fiserv.com>
2024-11-07 14:49:25 -0300

    Configurando React Query / Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import "./global.css";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </HelmetProvider>
   );
 }
- 
